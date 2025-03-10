9cdb611d5c2c524c8568ec822017566d8e04bf89
BrousseWayne <samynedir@gmail.com>
2025-03-10 16:05:15 +0100

    add routes

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,16 @@ dotenv.config({ path: '/Users/samy/Projects/testBackendApi/conf.env' })
 const PORT = process.env.PORT
 const app = express()
 
+app.get("/", (req, res) => {
+    res.send("Welcome to the Pokémon API!");
+});
+
+app.get("/search", (req, res) => {
+    const { name } = req.query;
+    res.send(`Searching for Pokémon: ${name}`);
+});
+
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
 })
+

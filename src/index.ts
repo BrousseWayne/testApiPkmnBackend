86d2cd62e939caeef29e4675ff06b25b24e6f03f
BrousseWayne <samynedir@gmail.com>
2025-05-10 19:58:54 +0200

    add new route

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import type { Request, Response } from "express"
 import dotenv from "dotenv"
 import helmet from "helmet"
 import { validateAndSanitizeQueryString } from "./middlewares.ts"
-import { fetchByName, fetchByType } from "./fetchPokemon.ts"
+import { fetchByMove, fetchByName, fetchByType } from "./fetchPokemon.ts"
 import cors from "cors"
 
 dotenv.config({ path: '/Users/samy/Projects/testBackendApi/conf.env' })
@@ -36,8 +36,9 @@ async function cachedFetchByName(name: string) {
     return data;
 }
 
+app.use(express.json());
 
-app.get("/search", validateAndSanitizeQueryString(["name", "type"]), async (req: Request, res: Response) => {
+app.get("/pokemon", validateAndSanitizeQueryString(["name", "type"]), async (req: Request, res: Response) => {
     try {
         if (req.query.type) {
             const result = await fetchByType(req.query.type as string);
@@ -65,6 +66,43 @@ app.get("/search", validateAndSanitizeQueryString(["name", "type"]), async (req:
     }
 });
 
+// "searchBy: type, statAttack > 50, "
+// "damage-class", "name", "power", "type"
+
+app.post("/moves", async (req: Request, res: Response) => {
+    console.log(req.body)
+    const { moveName, powerOperator, movePower, moveType, damageClass } = req.body
+    if (moveName) {
+        const result = await fetchByMove(moveName)
+        console.log(result)
+        res.json(result)
+    }
+    // try {
+    //     if (req.query.type) {
+    //         const result = await fetchByType(req.query.type as string);
+
+    //         const allPkmn = await Promise.all(
+    //             result.pokemon.map(async (entry) => {
+    //                 try {
+    //                     return await cachedFetchByName(entry.pokemon.name);
+    //                 } catch (err) {
+    //                     console.error(`Failed to fetch ${entry.pokemon.name}:`, err);
+    //                     return null; // or a fallback object
+    //                 }
+    //             })
+    //         );
+
+    //         const successfulPkmn = allPkmn.filter(p => p !== null);
+    //         res.json(successfulPkmn);
+    //     } else {
+    //         res.json(await cachedFetchByName(req.query.name as string));
+    //     }
+    // } catch (err) {
+    //     const status = err.status || 500;
+    //     const message = err.message || "Failed to fetch Pokémon";
+    //     res.status(status).json({ error: message });
+    // }
+});
 
 
 app.listen(PORT, () => {
@@ -73,4 +111,5 @@ app.listen(PORT, () => {
 
 
 //TODO: Add caching with redis
+//TODO: change endpoints names
 //TODO: Add database
